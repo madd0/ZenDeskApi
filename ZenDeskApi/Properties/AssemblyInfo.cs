e52f15d7089af0922df458c55bc12bc95cd8da06
Mauricio DIAZ ORLICH <mauricio.diazorlich@gmail.com>
2011-11-17 15:15:05 +0100

    Cleaned AssemblyInfo.cs

--- ZenDeskApi/Properties/AssemblyInfo.cs
+++ ZenDeskApi/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ZenDeskApi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ZenDeskApi")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
