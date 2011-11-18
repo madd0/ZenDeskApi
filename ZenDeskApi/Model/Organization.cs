69d4f434df2a407a72634cc3840b1990f76ae471
Mauricio DIAZ ORLICH <mauricio.diazorlich@gmail.com>
2011-11-18 17:35:09 +0100

    Added support for tags in organizations, as well as other fields

--- ZenDeskApi/Model/Organization.cs
+++ ZenDeskApi/Model/Organization.cs
@@ -9,7 +9,6 @@ namespace ZenDeskApi.Model
     [ZenDeskSerialization(Name = "organization")]
     public class Organization
     {
-
         [ZenDeskSerialization(Name = "id")]
         public int Id { get; set; }
 
@@ -24,5 +23,29 @@ namespace ZenDeskApi.Model
 
         [ZenDeskSerialization(Name = "users")]
         public List<User> Users { get; set; }
+
+        [ZenDeskSerialization(Name = "set-tags")]
+        public string SetTags { get; set; }
+
+        [ZenDeskSerialization(Name = "created-at")]
+        public DateTime CreatedAt { get; set; }
+
+        [ZenDeskSerialization(Name = "group-id")]
+        public int GroupId { get; set; }
+        
+        [ZenDeskSerialization(Name = "is-shared-comments")]
+        public bool IsSharedComments { get; set; }
+        
+        [ZenDeskSerialization(Name = "notes")]
+        public string Notes { get; set; }
+
+        [ZenDeskSerialization(Name = "suspended")]
+        public bool IsSuspended { get; set; }
+        
+        [ZenDeskSerialization(Name = "updated-at", Skip = true)]
+        public DateTime UpdatedAt { get; set; }
+        
+        [ZenDeskSerialization(Name = "current-tags", Skip = true)]
+        public string CurrentTags { get; set; }
     }
 }
